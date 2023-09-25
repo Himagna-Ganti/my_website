086cd9091ed7e71cf29a733a1a87d055696c2542
Himagna Ganti <himagna.ganti6@gmail.com>
2023-09-24 21:21:44 -0400

    Created flex for experience

---
+++ src/components/Experience.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Experience() {
+  return (
+    <div className="border-2 border-white w-[screen] mx-[40px] h-[100px] text-white">
+      <div className="flex justify-center">
+        <h1 className="">Experience</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Experience;

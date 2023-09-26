2d0a1e36ee8c3a9f8b629ac48edd99521f5d218c
Himagna Ganti <himagna.ganti6@gmail.com>
2023-09-25 23:00:16 -0400

    Finished the project card adn updated assets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Nav from "./components/Nav";
 import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
 
 function App() {
   return (
     <div className="bg-darkness overflow-hidden ">
       <Nav />
-      <hr className="mx-10 w-[94.5%] h-[0.5px] opacity-40 " />
+      <hr className="mx-10 w-[95%] h-[0.5px] opacity-40 " />
       <Header />
+      <Projects />
+      <Experience />
     </div>
   );
 }
